91be7ac738c55b859e39f11ce6ce89ca64c1fd40
dasar <wanjikuestivah@gmail.com>
2023-10-11 15:46:50 +0300

    fixing the reminder

--- script.js
+++ script.js
@@ -1,161 +1,257 @@
 
-const lastOperationScreen = document.getElementById('lastOperationScreen')
+
+
+
+
+
+function handleEquation(equation) {
+    console.log(equation)
+    equation = equation.split(" ");
+	const operators = ['/', 'x', '+', '-'];
+	let firstNumber;
+	let secondNumber;
+	let operator;
+	let operatorIndex;
+	let result;
+    //let newCurrentOperationScreen = '';
+    //let action = '';
+
+	/*  
+		1. Perform calculations as per BODMAS Method
+		2. For that use operators array
+		3. after calculation of 1st numbers replace them with result
+		4. use splice method
+
+	*/
+    //if (equation.includes('%')) {
+    //    console.log('was here')
+       /*  currentOperationScreen.textContent = inputDisplay + keyValue;
+        equation = equation + key.value;
+        checkForRemainder = checkForRemainder + keyValue;
+        firstNumber = (parseFloat(firstNumber)/100);
+        firstNumber = firstNumber.toString();
+        currentOperationScreen.textContent = firstNumber; */
+    //}   
+	for (var i = 0; i < operators.length; i++) {
+		while (equation.includes(operators[i])) {
+			operatorIndex = equation.findIndex(item => item === operators[i])
+			firstNumber = equation[operatorIndex-1]
+			operator = equation[operatorIndex]
+			secondNumber = equation[operatorIndex+1]
+            if(firstNumber.includes('%')  ) {
+                firstNumber = (parseFloat(firstNumber)/100)
+                firstNumber = firstNumber.toString()
+            }else if(secondNumber.includes('%')) {
+                secondNumber = (parseFloat(secondNumber)/100)
+                secondNumber = secondNumber.toString()
+            }
+
+            console.log('first number', firstNumber, 'operator', operator, 'secondNumber', secondNumber)
+            result = calculate(firstNumber, operator, secondNumber)
+			equation.splice(operatorIndex - 1, 3, result)
+		}
+	}
+
+	return result;
+    //currentOperation = null
+} 
+
+// Event Listener for keyboard button press
+document.addEventListener('keydown', (event) => {
+	
+	let getOperators = {
+		'/': 'divide',
+		'x': 'multiply',
+		'*': 'multiply',
+		'+': 'plus',
+		'-': 'minus'
+	}
+    console.log()
+
+	if(!isNaN(event.key) && event.key !== ' '){
+		document.getElementById(`number-${event.key}`).click();
+	}
+	if (['x', '+', '-', '*', '/'].includes(event.key)) {
+		document.getElementById(getOperators[event.key]).click();
+        console.log(event.key.textContent)
+	}
+	if (event.key === 'Backspace' || event.key ==='c' || event.key === 'C') {
+		document.getElementById('clear').click();	
+	}
+	if (event.key === '=' || event.key === 'Enter') {
+		document.getElementById('equals').click();	
+	}
+	if (event.key === '.') {
+		document.getElementById('decimal').click();	
+	}
+    if (event.key === '%') {
+		document.getElementById('remainder').click();	
+	}
+});
+
+
+
+const calcKeys = document.querySelector('.buttons-flex');
 const currentOperationScreen = document.getElementById('currentOperationScreen')
-const numberButtons = document.querySelectorAll('[data-number]')
-const operatorButtons = document.querySelectorAll('[data-operator]') 
-const equalsButton = document.getElementById('equalsBtn')
-const clearButton = document.getElementById('clearBtn')
-const allClearButton = document.getElementById('allclearBtn')
-const decimalButton = document.getElementById('decimalBtn')
-const remainderButton = document.getElementById('remainder')
+const displayLastOperationScreen = document.getElementById('lastOperationScreen')
+const calculator = document.querySelector('.calculator');
+let isEqualsPressed = false;
+let equation = 0; //separate variable to calculate equation in backend
+let checkForDecimal = ''; //to store each number and check if decimal is pressed
+//let checkForRemainder = '';
 
-/* let firstNumber = ''
-let secondNumber = '' */
-let currentOperation = null
-let shouldResetScreen = false
+calcKeys.addEventListener('click', (event) => {
+
+	//Check if click is on the button and not on the container
+	if(!event.target.closest('button')) return;
+
+	const key = event.target;
+	let keyValue = key.textContent;
+    console.log(keyValue)
+    let inputDisplay = currentOperationScreen.textContent;
+	const { type } = key.dataset;
+	const { previousKeyType } = calculator.dataset;
+
+    if(isEqualsPressed) {
+        isEqualsPressed = false;
+        inputDisplay = displayLastOperationScreen.textContent;
+        equation = displayLastOperationScreen.textContent;
+        displayLastOperationScreen.innerHTML = '&nbsp;'
 
 
-window.addEventListener('keydown', handleKeyboardInput)
-equalsButton.addEventListener('click', evaluate)
-decimalButton.addEventListener('click', appendPoint)
-remainderButton.addEventListener('click', appendPercentage)
-allClearButton.addEventListener('click', clear)
-clearButton.addEventListener('click', deleteNumber)
+    }	
+	//If any number button is pressed
+	if(type === 'number' && !isEqualsPressed) {
+		/*
+			1. Inital screen display is 0
+			2. replace initial display with user input if number is pressed
+			3. else concat with operator
+			4. if screen display is anything other than number concat the display
+		*/
+		if (inputDisplay === '0') {
+			currentOperationScreen.textContent = (previousKeyType === 'operator') ? inputDisplay + keyValue : keyValue;
+			equation = (previousKeyType === 'operator') ? equation + key.value : key.value;
+			checkForDecimal = checkForDecimal + keyValue;
+		}else {
+			//Check length so that number stays within display box
+			//else replace it with exponential
+			if (checkForDecimal.length >= 19) {
+				var replaceNumber = checkForDecimal;
+				checkForDecimal = Number(checkForDecimal).toExponential(2);
+				currentOperationScreen.textContent = inputDisplay.replace(replaceNumber, checkForDecimal);
+			}else {
+				//Check for Infinity OR NaN in Display
+				currentOperationScreen.textContent = currentOperationScreen.textContent.includes('N') ? 'NaN' : 
+										currentOperationScreen.textContent.includes('I') ? 'Infinity' : inputDisplay + keyValue;
+				equation = equation + key.value;
+				checkForDecimal = checkForDecimal + keyValue;
+                //checkForRemainder = checkForRemainder + keyValue;
 
-numberButtons.forEach((button) =>
-    button.addEventListener('click', () => appendNumber(button.textContent))
+			}
+		}
+	}
 
-)
-operatorButtons.forEach((button) =>
-  button.addEventListener('click', () => setOperation(button.textContent))
-)
-function appendNumber(number) {
-    if (currentOperationScreen.textContent === '0' || shouldResetScreen)
-      resetScreen()
-    currentOperationScreen.textContent += number
-}
-  
-function resetScreen() {
-    currentOperationScreen.textContent = ''
-    shouldResetScreen = false
-}
-function clear() {
-    currentOperationScreen.textContent = '0'
-    lastOperationScreen.textContent = ''
-    firstOperand = ''
-    secondOperand = ''
-    currentOperation = null
-}
-function appendPoint() {
-    if (shouldResetScreen) resetScreen()
-    if (currentOperationScreen.textContent === '')
-      currentOperationScreen.textContent = '0'
-    if (currentOperationScreen.textContent.includes('.')) return
-    currentOperationScreen.textContent += '.'
-} 
-function appendPercentage() {
-    if (shouldResetScreen) resetScreen()
-    if (currentOperationScreen.textContent === '')
-      currentOperationScreen.textContent = '0'
-    if (currentOperationScreen.textContent.includes('%')) return
-    currentOperationScreen.textContent += '%'
-}
+	/*
+		1. Check if operator is pressed AND Equals To (=) is not yet pressed
+		2. AND Display dose not include Infinity
+		3. Replace checkForDecimal with blank to store next number
+	*/
+    if (type === 'operator' && previousKeyType !== 'operator'
+    && !isEqualsPressed && !inputDisplay.includes('Infinity')) {
+        console.log("input:", inputDisplay, "keyval", keyValue, "equ", equation)
+    //calculator.dataset.firstNumber = checkForDecimal;
+    // calculator.dataset.operator = key.id;
+    checkForDecimal = '';
+    currentOperationScreen.textContent = inputDisplay + ' ' + keyValue + ' ';
+    equation = equation + ' ' + key.value + ' ';
 
-function deleteNumber() {
-    currentOperationScreen.textContent = currentOperationScreen.textContent
-      .toString()
-      .slice(0, -1)
-}
-function setOperation(operator) {
-    if (currentOperation != null) evaluate()
-    firstOperand = currentOperationScreen.textContent
-    console.log(firstOperand)
-    currentOperation = operator
-    console.log(currentOperation)
-    lastOperationScreen.textContent = `${firstOperand} ${currentOperation}`
-    shouldResetScreen = true
-}
-function evaluate() {
-    if (currentOperation === null || shouldResetScreen) return
-    if (currentOperation === '÷' && currentOperationScreen.textContent === '0') {
-      alert("You can't divide by 0!")
-      return
     }
-    secondOperand = currentOperationScreen.textContent
-    tempFirstOperand = firstOperand
-    tempSecondOperand = secondOperand
-    console.log(firstOperand)
-    console.log(secondOperand)
-    console.log(is_percentage(firstOperand))
-    if(is_percentage(firstOperand)== true) {
-        firstOperand=firstOperand.slice(0,-1)
-        console.log(firstOperand)
-        firstOperand=(parseFloat(firstOperand)/100)
-        console.log(firstOperand)
-    }
-    else if(is_percentage(secondOperand) == true){
-        secondOperand=secondOperand.slice(0,-1)
-        console.log(secondOperand)
-        secondOperand=(parseFloat(secondOperand)/100)
-        console.log(secondOperand)
-    }
-    currentOperationScreen.textContent = roundResult(
-      operate(currentOperation, firstOperand, secondOperand)
-    )
-    lastOperationScreen.textContent = `${tempFirstOperand} ${currentOperation} ${tempSecondOperand} =`
-    currentOperation = null
-}
-function roundResult(number) {
-    return Math.round(number * 1000) / 1000
-} 
-function handleKeyboardInput(e) {
-    if (e.key >= 0 && e.key <= 9) appendNumber(e.key)
-    if (e.key === '.') appendPoint()
-    if (e.key === '=' || e.key === 'Enter') evaluate()
-    if (e.key === 'Backspace') deleteNumber()
-    if (e.key === 'Escape') clear()
-    if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/' || e.key === '%')
-      setOperation(convertOperator(e.key))
-}
-function convertOperator(keyboardOperator) {
-    if (keyboardOperator === '/') return '÷'
-    if (keyboardOperator === '*') return 'x'
-    if (keyboardOperator === '-') return '-'
-    if (keyboardOperator === '+') return '+'
-}
-function add(a, b) {
-    return a + b
-}
-  
-function substract(a, b) {
-    return a - b
-}
-  
-function multiply(a, b) {
-    return a * b
-}
-  
-function divide(a, b) {
-    return a / b
-}
 
- function is_percentage(a) {
-    return a.endsWith('%')
-} 
-function operate(operator, a, b) {
-    a = Number(a)
-    b = Number(b)
-    switch (operator) {
-      case '+':
-        return add(a, b)
-      case '-':
-        return substract(a, b)
-      case 'x':
-        return multiply(a, b)
-      case '÷':
-        if (b === 0) return null
-        else return divide(a, b)
-      default:
-        return null
+    /*
+        1. Check if Decimal button is pressed AND Equals To (=) is not yet pressed
+        2. AND was a previously pressed button a number or was display a 0
+        3. #2 required so that if user presses decimal after operator, it is not displayed
+        4. check if the number already contains a decimal
+    */
+    if (type === 'decimal' && (previousKeyType === 'number' || inputDisplay === '0')
+        && !isEqualsPressed && !inputDisplay.includes('Infinity')) {
+        if (!checkForDecimal.includes('.')) {
+            currentOperationScreen.textContent = inputDisplay + keyValue;
+            equation = equation + key.value;
+            checkForDecimal = checkForDecimal + keyValue;
+        }
+    }
+
+    if ((type === 'backspace' || type === 'reset') && inputDisplay !== '0') {
+        if (type === 'backspace' && !isEqualsPressed) {
+            currentOperationScreen.textContent = inputDisplay.substring(0, inputDisplay.length - 1);
+            equation = equation.substring(0, equation.length - 1);
+            checkForDecimal = checkForDecimal.substring(0, checkForDecimal.length - 1);
+        } else {
+            inputDisplay = '0';
+            currentOperationScreen.textContent = inputDisplay;
+            displayLastOperationScreen.innerHTML = '&nbsp;';
+            isEqualsPressed = false;
+            equation = '';
+            checkForDecimal = '';
+        }
+
+    }
+    if (type === 'remainder' && (previousKeyType === 'number' || inputDisplay === '0')
+    && !isEqualsPressed && !inputDisplay.includes('Infinity')) {
+    if (!checkForRemainder.includes('%')) {
+        currentOperationScreen.textContent = inputDisplay + keyValue;
+        equation = equation + key.value;
+        checkForRemainder = checkForRemainder + keyValue;
     }
 }
+
+if ((type === 'backspace' || type === 'reset') && inputDisplay !== '0') {
+    if (type === 'backspace' && !isEqualsPressed) {
+        currentOperationScreen.textContent = inputDisplay.substring(0, inputDisplay.length - 1);
+        equation = equation.substring(0, equation.length - 1);
+        checkForRemainder = checkForRemainder.substring(0, checkForRemainder.length - 1);
+    } else {
+        inputDisplay = '0';
+        currentOperationScreen.textContent = inputDisplay;
+        displayLastOperationScreen.innerHTML = '&nbsp;';
+        isEqualsPressed = false;
+        equation = '';
+        checkForRemainder = '';
+    }
+
+}
+    //Send equation for calculation after Equals To (=) is pressed
+    if (type === 'equal') {
+        // Perform a calculation
+        isEqualsPressed = true;
+        const finalLastOperationScreen = handleEquation(equation);
+        
+        if (finalLastOperationScreen || finalLastOperationScreen === 0) {
+            displayLastOperationScreen.textContent = (!Number.isInteger(finalLastOperationScreen)) ? finalLastOperationScreen.toFixed(2) : 
+                                        (finalLastOperationScreen.toString().length >= 16) ? finalLastOperationScreen.toExponential(2) : finalLastOperationScreen ;
+        } else {
+            displayLastOperationScreen.textContent = 'Math Error';
+        }
+        //isEqualsPressed =false;
+    }
+    calculator.dataset.previousKeyType = type;
+})
+
+
+//Function to calculate result based on each operator
+function calculate(firstNumber, operator, secondNumber) {
+
+	firstNumber = Number(firstNumber);
+	secondNumber = Number(secondNumber);
+
+    if (operator === 'plus' || operator === '+') return firstNumber + secondNumber;
+    if (operator === 'minus' || operator === '-') return firstNumber - secondNumber;
+    if (operator === 'multiply' || operator === 'x') return firstNumber * secondNumber;
+    if (operator === 'divide' || operator === '/') return firstNumber / secondNumber;
+   // if (operator === 'remainder' || operator === '%') return firstNumber % secondNumber;
+
+}
+
+
+    
